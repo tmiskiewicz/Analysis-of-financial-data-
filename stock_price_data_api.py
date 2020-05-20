e0aea6f1a4fd3848e58f9cda2028dbc95ed7cb8c
tmiskiewicz <32105258+tmiskiewicz@users.noreply.github.com>
2020-05-20 22:34:27 +0200

    Update stock_price_data_api.py

--- stock_price_data_api.py
+++ stock_price_data_api.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 url = 'https://www.bankier.pl/gielda/notowania/akcje?index=WIG'
 page = requests.get(url)
 
-soup = BeautifulSoup(page.content,'lxml')
+soup = BeautifulSoup(page.content,'html.parser')
 
 # Find the second table on the page
 t = soup.find_all('table')[0]
@@ -21,19 +21,33 @@ results = pd.DataFrame()
 ###############################################################################
 #get data about Price, Volume, High, Low, Close and Open prices of stocks
 
-for names in names_of_company:
-    quandl.ApiConfig.api_key = 'api_key'
-    x = quandl.get('WSE/%s' %names, start_date='2019-01-17', 
-    end_date='2019-01-24',
-    paginate=True)
-    x['company'] = names
-    results = results.append(x)
+#remove NaN
+for i in range(len(names_of_company)):
+    names_of_company[i] = str(names_of_company[i])
+
+names_of_company_without_nan = [x for x in names_of_company if x != 'nan']
+
+for names in names_of_company_without_nan:
+    quandl.ApiConfig.api_key = 'AZ964MpikzEYAyLGfJD2'
+    try:
+        x = quandl.get('WSE/%s' %names, start_date='2019-01-17',     end_date='2019-01-24',    paginate=True)
+        x['company'] = names
+        results = results.append(x)
+    #Jeśli wywali się w tym 'try' to bierze następny w kolejce i leci dalej
+    except:
+        next
 
 results = results.reset_index(drop=False)
-pd.DataFrame.to_csv(results, 'WIG.csv')
+pd.DataFrame.to_csv(results, 'WIG_COMPANY.csv')
+
 
 #download data for WIG stock index
-quandl.ApiConfig.api_key = 'api_key'
+quandl.ApiConfig.api_key = 'AZ964MpikzEYAyLGfJD2'
+wig20 = quandl.get("WSE/WIG20")
+wig20 = pd.DataFrame(wig20)
+
+pd.DataFrame.to_csv(wig20, 'WIG20.csv')
+
 wig = quandl.get("WSE/WIG")
 wig = pd.DataFrame(wig)
 
